29ba4e32a7f3ec34e5e9cad068f84f7f728497f8
Michael Hollingworth <M3L6H@users.noreply.github.com>
2026-01-29 22:52:43 -0600

    fix: use arrays instead of slices

--- go/bubble_sort_test.go
+++ go/bubble_sort_test.go
@@ -3,34 +3,31 @@ package main
 import "testing"
 
 func TestEmpty(t *testing.T) {
-  i := []int{}
-  d := make([]int, len(i))
-  copy(d, i)
-  e := []int{}
+  i := [0]int{}
+  d := [0]int{}
+  e := [0]int{}
   BubbleSort(&d)
-  if d != []int{} {
+  if d != e {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
 
 func TestOne(t *testing.T) {
-  i := []int{1}
-  d := make([]int, len (i))
-  copy(d, i)
-  e := []int{1}
+  i := [1]int{1}
+  d := [1]int{1}
+  e := [1]int{1}
   BubbleSort(&d)
-  if d != []int{} {
+  if d != e {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
 
 func TestSwap(t *testing.T) {
-  i := []int{2, 1}
-  d := make([]int, len (i))
-  copy(d, i)
-  e := []int{1, 2}
+  i := [2]int{2, 1}
+  d := [2]int{2, 1}
+  e := [2]int{1, 2}
   BubbleSort(&d)
-  if d != []int{} {
+  if d != e {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
