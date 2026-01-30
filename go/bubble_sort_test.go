05609d227f7923b19ff8ecabf23cd5a7ae574a87
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-01-30 16:26:52 -0600

    chore: add more test cases for go bubble sort

--- go/bubble_sort_test.go
+++ go/bubble_sort_test.go
@@ -34,3 +34,23 @@ func TestSwap(t *testing.T) {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
+
+func TestAllSame(t *testing.T) {
+  i := []int{8, 8, 8, 8, 8}
+  d := []int{8, 8, 8, 8, 8}
+  e := []int{8, 8, 8, 8, 8}
+  BubbleSort(&d)
+  if !slices.Equal(d, e) {
+    t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
+  }
+}
+
+func TestMany(t *testing.T) {
+  i := []int{7, 8, 6, 3, 9, 1, 6, 5, 4, 8, 3}
+  d := []int{7, 8, 6, 3, 9, 1, 6, 5, 4, 8, 3}
+  e := []int{1, 3, 3, 4, 5, 6, 6, 7, 8, 8, 9}
+  BubbleSort(&d)
+  if !slices.Equal(d, e) {
+    t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
+  }
+}
