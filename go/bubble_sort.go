0cdaf327f6e2640fe2d8d7d5687134e6b9e9f4ad
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-01-30 16:37:50 -0600

    feat: implement bubble sort in go

--- go/bubble_sort.go
+++ go/bubble_sort.go
@@ -1,4 +1,18 @@
 package main
 
-func BubbleSort[T Ordered](arr *[]T) {
+func BubbleSort[T Ordered](s *[]T) {
+  sorted := false
+  for !sorted {
+    sorted = true
+    for i, v := range *s {
+      if i == len(*s) - 1 {
+        break
+      }
+
+      if v > (*s)[i + 1] {
+        (*s)[i], (*s)[i + 1] = (*s)[i + 1], (*s)[i]
+        sorted = false
+      }
+    }
+  }
 }
