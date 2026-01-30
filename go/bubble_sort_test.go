466dce75cadb6ff11dab7be6fbb8a219159669ed
Michael Hollingworth <M3L6H@users.noreply.github.com>
2026-01-29 22:57:17 -0600

    fix: use slices

--- go/bubble_sort_test.go
+++ go/bubble_sort_test.go
@@ -1,33 +1,36 @@
 package main
 
-import "testing"
+import (
+  "slices"
+  "testing"
+) 
 
 func TestEmpty(t *testing.T) {
-  i := [0]int{}
-  d := [0]int{}
-  e := [0]int{}
+  i := []int{}
+  d := []int{}
+  e := []int{}
   BubbleSort(&d)
-  if d != e {
+  if !slices.Equal(d, e) {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
 
 func TestOne(t *testing.T) {
-  i := [1]int{1}
-  d := [1]int{1}
-  e := [1]int{1}
+  i := []int{1}
+  d := []int{1}
+  e := []int{1}
   BubbleSort(&d)
-  if d != e {
+  if !slices.Equal(d, e) {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
 
 func TestSwap(t *testing.T) {
-  i := [2]int{2, 1}
-  d := [2]int{2, 1}
-  e := [2]int{1, 2}
+  i := []int{2, 1}
+  d := []int{2, 1}
+  e := []int{1, 2}
   BubbleSort(&d)
-  if d != e {
+  if !slices.Equal(d, e) {
     t.Errorf("BubbleSort(%v) = %v; want %v", i, d, e);
   }
 }
